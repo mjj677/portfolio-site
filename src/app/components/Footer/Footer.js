35d2ecc11cabfa84deea5c21f0cc5d16210da766
Matt Johnston <mattjohnston2000@gmail.com>
2024-07-17 16:05:32 +0100

    adds working contact section and media queries across all components

---
+++ src/app/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import styles from "./FooterStyles.module.css"
+
+
+export const Footer = () => {
+  return (
+    <section id="footer" className={styles.container}>
+        <p>&copy; 2024 Matthew Johnston <br />
+        All rights reserved <br />
+        Built with Next.js and JavaScript
+        </p>
+    </section>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import styles from "./styles/page.module.css";
 import { Hero } from "./components/Hero/Hero.js";
 import { Projects } from "./components/Projects/Projects";
 import { Stack } from "./components/Stack/Stack";
+import { ContactForm } from "./components/ContactForm/ContactForm";
+import { Footer } from "./components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <Hero />
       <Projects />
       <Stack />
+      <ContactForm />
+      <Footer />
     </main>
   );
 }
